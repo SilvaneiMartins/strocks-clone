399e737b797dfcece992f59f5872c2ab6a01ea4d
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-16 11:50:04 -0400

    Initial Commit

---
+++ strocks-clone/strocks_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  strocks_cloneApp.swift
+//  strocks-clone
+//
+//  Created by Silvanei  Martins on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct strocks_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
